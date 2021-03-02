92fcb1b980642333e89c3f1750345d689ecc996c
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-03-02 16:40:57 -0300

    movies model, controller and view

--- Vidly/Properties/AssemblyInfo.cs
+++ Vidly/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
-[assembly: Guid("769a2237-3939-4711-976a-b48f55c7511f")]
+[assembly: Guid("b3876a5c-e961-4742-b5ea-f8902ae723df")]
 
 // As informações de versão de um assembly consistem nos seguintes quatro valores:
 //

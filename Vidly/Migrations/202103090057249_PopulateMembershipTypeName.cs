28d68f7e21ba796483d9f7bd8f7c29a6fef56f9f
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-03-08 22:00:23 -0300

    membershipType Name column

---
+++ Vidly/Migrations/202103090057249_PopulateMembershipTypeName.cs
@@ -0,0 +1,24 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateMembershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE dbo.MembershipTypes SET Name = 'Pay as You Go' where DurationInMonths = 0");
+            Sql("UPDATE dbo.MembershipTypes SET Name = 'Monthly' where DurationInMonths = 1");
+            Sql("UPDATE dbo.MembershipTypes SET Name = 'Quarterly' where DurationInMonths = 3");
+            Sql("UPDATE dbo.MembershipTypes SET Name = 'Yearly' where DurationInMonths = 12");
+        }
+
+        public override void Down()
+        {
+            Sql("UPDATE dbo.MembershipTypes SET Name = NULL where DurationInMonths = 0");
+            Sql("UPDATE dbo.MembershipTypes SET Name = NULL where DurationInMonths = 1");
+            Sql("UPDATE dbo.MembershipTypes SET Name = NULL where DurationInMonths = 3");
+            Sql("UPDATE dbo.MembershipTypes SET Name = NULL where DurationInMonths = 12");
+        }
+    }
+}
